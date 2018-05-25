aee8821c4f9a8f3ab8364b0876658387d29b0b05
bartosz <zwolak311@gmail.com>
2018-05-26 00:29:04 +0200

    Missing class added

---
+++ app/src/main/java/com/bartosz/bartoszzwoliski/API/MyRetrofit.java
@@ -0,0 +1,107 @@
+package com.bartosz.bartoszzwoliski.API;
+
+import android.support.annotation.NonNull;
+
+import com.bartosz.bartoszzwoliski.POJO.LeagueSimpleNamePOJO;
+import com.bartosz.bartoszzwoliski.POJO.LeagueTablePOJO;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.util.ArrayList;
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class MyRetrofit {
+    private Api api;
+    private LeagueListInterface leagueListInterface;
+    private LeagueTableInterface leagueTableInterface;
+
+    public MyRetrofit(LeagueListInterface leagueListInterface){
+        this.leagueListInterface = leagueListInterface;
+        setupApi();
+    }
+
+
+    public MyRetrofit(LeagueTableInterface leagueTableInterface){
+        this.leagueTableInterface = leagueTableInterface;
+        setupApi();
+    }
+
+    private void setupApi(){
+        HttpLoggingInterceptor logging = new HttpLoggingInterceptor();
+        logging.setLevel(HttpLoggingInterceptor.Level.BODY);
+
+
+        OkHttpClient.Builder okHttpClient = new OkHttpClient.Builder();
+        okHttpClient.addInterceptor(logging);
+        okHttpClient.readTimeout(5, TimeUnit.SECONDS);
+        okHttpClient.connectTimeout(5, TimeUnit.SECONDS);
+
+
+        Gson gson = new GsonBuilder()
+                .setLenient()
+                .create();
+
+
+        Retrofit builder = new Retrofit.Builder()
+                .baseUrl("http://api.football-data.org/")
+                .client(okHttpClient.build())
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .build();
+
+        api = builder.create(Api.class);
+    }
+
+
+    public void getLeagueList(){
+
+
+        Call<ArrayList<LeagueSimpleNamePOJO>> callLeagueList = api.getLeagueList();
+
+        callLeagueList.clone().enqueue(new Callback<ArrayList<LeagueSimpleNamePOJO>>() {
+            @Override
+            public void onResponse(@NonNull Call<ArrayList<LeagueSimpleNamePOJO>> call, @NonNull Response<ArrayList<LeagueSimpleNamePOJO>> response) {
+
+                leagueListInterface.onResponse(response.body());
+
+            }
+
+            @Override
+            public void onFailure(@NonNull Call<ArrayList<LeagueSimpleNamePOJO>> call, @NonNull Throwable t) {
+                leagueListInterface.onFailure(t.getMessage());
+
+            }
+        });
+
+    }
+
+
+    public void getLeagueTable(String leagueId){
+
+        Call<LeagueTablePOJO> callLeagueTable = api.getLeagueTable(leagueId);
+
+        callLeagueTable.clone().enqueue(new Callback<LeagueTablePOJO>() {
+            @Override
+            public void onResponse(@NonNull Call<LeagueTablePOJO> call, @NonNull Response<LeagueTablePOJO> response) {
+
+                leagueTableInterface.onResponse(response.body());
+
+            }
+
+            @Override
+            public void onFailure(@NonNull Call<LeagueTablePOJO> call, @NonNull Throwable t) {
+
+                leagueTableInterface.onFailure(t.getMessage());
+
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/com/bartosz/bartoszzwoliski/POJO/LeagueTablePOJO.java
@@ -0,0 +1,105 @@
+package com.bartosz.bartoszzwoliski.POJO;
+
+import java.util.ArrayList;
+
+public class LeagueTablePOJO {
+
+    String leagueCaption;
+    ArrayList<Standing> standing = new ArrayList<>();
+
+
+    public String getLeagueCaption() {
+        return leagueCaption;
+    }
+
+    public void setLeagueCaption(String leagueCaption) {
+        this.leagueCaption = leagueCaption;
+    }
+
+    public ArrayList<Standing> getStanding() {
+        return standing;
+    }
+
+    public void setStanding(ArrayList<Standing> standing) {
+        this.standing = standing;
+    }
+
+    public class Standing {
+        String teamName, points, playedGames, teamId, wins, draws, crestURI;
+        int losses, position;
+
+        public int getPosition() {
+            return position;
+        }
+
+        public void setPosition(int position) {
+            this.position = position;
+        }
+
+        public String getTeamName() {
+            return teamName;
+        }
+
+        public void setTeamName(String teamName) {
+            this.teamName = teamName;
+        }
+
+        public String getPoints() {
+            return points;
+        }
+
+        public void setPoints(String points) {
+            this.points = points;
+        }
+
+        public String getPlayedGames() {
+            return playedGames;
+        }
+
+        public void setPlayedGames(String playedGames) {
+            this.playedGames = playedGames;
+        }
+
+        public String getTeamId() {
+            return teamId;
+        }
+
+        public void setTeamId(String teamId) {
+            this.teamId = teamId;
+        }
+
+
+        public String getWins() {
+            return wins;
+        }
+
+        public void setWins(String wins) {
+            this.wins = wins;
+        }
+
+        public String getDraws() {
+            return draws;
+        }
+
+        public void setDraws(String draws) {
+            this.draws = draws;
+        }
+
+        public int getLosses() {
+            return losses;
+        }
+
+        public void setLosses(int losses) {
+            this.losses = losses;
+        }
+
+        public String getCrestURI() {
+            return crestURI;
+        }
+
+        public void setCrestURI(String crestURI) {
+            this.crestURI = crestURI;
+        }
+    }
+
+}
